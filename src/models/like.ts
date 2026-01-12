6d2b20e52f5a88eba06b63c6de1e63688a13472a
PK <krzysztofpielot@gmail.com>
2026-01-12 15:12:46 +0100

    Added models: comment, like, meme and tag

---
+++ src/models/like.ts
@@ -0,0 +1,5 @@
+export interface Like{
+  id: string
+  userId: string
+  createdAt: string
+}
---
+++ src/models/meme.ts
@@ -0,0 +1,14 @@
+import type { Tag } from './tag'
+import type { Comment } from './comment'
+
+export interface Meme{
+  id: string
+  title: string
+  imageUrl: string
+  createdAt: string
+  userId: string
+  userName: string
+  tags: Tag[]
+  comments: Comment[]
+  likeCount: number
+}
